3d4b676223a8ebc8439fd87d616525d185303eee
feritarou <schwarz.f@web.de>
2022-05-26 19:19:14 +0200

    fix plugin setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-export * from './src/components';
+export * from './src/components'
+
+import Formula from './src/components/formula'
+import Geogebra from './src/components/geogebra'
+import MapGeogebra from './src/components/mapgeogebra'
+import Openstreetmap from './src/components/openstreetmap'
+import Worksheet from './src/components/worksheet'
+
+export default {
+  install (Vue) {
+    Vue.component('Formula', Formula)
+    Vue.component('Geogebra', Geogebra)
+    Vue.component('MapGeogebra', MapGeogebra)
+    Vue.component('Openstreetmap', Openstreetmap)
+    Vue.component('Worksheet', Worksheet)
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,3 @@ export * from "./geogebra";
 export * from "./worksheet";
 export * from "./openstreetmap";
 export * from "./map-geogebra";
-
-export default {
-  install (Vue) {
-    Vue.component('Formula', Formula)
-    Vue.component('Geogebra', Geogebra)
-    Vue.component('MapGeogebra', MapGeogebra)
-    Vue.component('Openstreetmap', Openstreetmap)
-    Vue.component('Worksheet', Worksheet)
-  }
-}
