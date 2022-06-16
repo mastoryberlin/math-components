bcda15cfa3a225f70554b6d07c650bde5d171c2d
feritarou <schwarz.f@web.de>
2022-06-17 00:35:23 +0200

    improve custom tools

--- src/components/mastory-geogebra/custom-tools.js
+++ src/components/mastory-geogebra/custom-tools.js
@@ -1,5 +1,6 @@
 export const customTools = [
-  {id: 100001, name: 'add_inequality', src: 'https://storage.googleapis.com/content_storage/shared/add_inequality.png'},
-  {id: 100002, name: 'delete_inequality', src: 'https://storage.googleapis.com/content_storage/shared/delete_inequality.png'},
-  {id: 100003, name: 'destination', src: 'https://storage.googleapis.com/content_storage/shared/destination.png'},
+  // {id: 0, name: 'add_inequality', src: 'https://storage.googleapis.com/content_storage/shared/move.png', hint: 'Move'},
+  {id: 100001, name: 'add_inequality', src: 'https://storage.googleapis.com/content_storage/shared/add_inequality.png', hint: 'Cut Plane'},
+  {id: 100002, name: 'delete_inequality', src: 'https://storage.googleapis.com/content_storage/shared/delete_inequality.png', hint: 'Remove Cut'},
+  {id: 100003, name: 'destination', src: 'https://storage.googleapis.com/content_storage/shared/destination.png', hint: 'Suggest Destination'},
 ]
