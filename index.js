47251a80f798e2ab44cd72f50f423b60662371a0
feritarou <schwarz.f@web.de>
2022-06-16 23:44:31 +0200

    fix 2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 export * from './src/components'
 
 import Formula from './src/components/formula'
-import Geogebra from './src/components/geogebra'
+import VueGeogebra from './src/components/vue-geogebra'
+import MastoryGeogebra from './src/components/mastory-geogebra'
 import MapGeogebra from './src/components/map-geogebra'
 import Openstreetmap from './src/components/openstreetmap'
 import Worksheet from './src/components/worksheet'
@@ -9,7 +10,8 @@ import Worksheet from './src/components/worksheet'
 export default {
   install (Vue) {
     Vue.component('Formula', Formula)
-    Vue.component('Geogebra', Geogebra)
+    Vue.component('VueGeogebra', VueGeogebra)
+    Vue.component('MastoryGeogebra', MastoryGeogebra)
     Vue.component('MapGeogebra', MapGeogebra)
     Vue.component('Openstreetmap', Openstreetmap)
     Vue.component('Worksheet', Worksheet)
