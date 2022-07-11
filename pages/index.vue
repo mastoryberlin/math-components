9c0039bae8d16f1a2f2d66932d29e6dd5e6e3ef4
feritarou <schwarz.f@web.de>
2022-07-11 19:44:33 +0200

    fix resize + reload issues, hopefully for the last time

--- pages/index.vue
+++ pages/index.vue
@@ -3,37 +3,69 @@
     <h1>
       Mastory Math Components
     </h1>
-    <map-geogebra
-      v-model="ggb"
-      toolbar="move add_inequality"
-      :display-width="w"
-      :display-height="h"
-      :view-rect="{ x: [-20, 20], y: [-25, 25], contain: true }"
-      :allow-pan="{ x: [-100, 100], y: [-100, 100] }"
-      :enable-undo-redo="false"
-    >
-    </map-geogebra>
+
+    <div v-show="show">
+      <!-- :allow-pan="{ x: [-100, 100], y: [-100, 100] }" -->
+      <map-geogebra
+        v-model="ggb"
+        toolbar="move add_inequality"
+        :display-width="w"
+        :display-height="h"
+        :view-rect="{ x: [-20, 20], y: [-20, 20], contain: true }"
+        :enable-undo-redo="false"
+        @load="onResize"
+      />
+      <button @click="scheduleShowHide(false)">
+        Hide in 1 seconds
+      </button>
+    </div>
+    <button v-show="!show" @click="scheduleShowHide(true)">
+      Show in 1 seconds
+    </button>
   </div>
 </template>
 
 <script>
+import {mapState, mapMutations} from 'vuex'
 export default {
   data: () => ({
     ggb: {api: null},
-    w: 300,
-    h: 300,
+    w: process.client? window.innerWidth - 30 : 300,
+    h: process.client? window.innerHeight -120 : 300,
+    show: false,
   }),
-  mounted() {
-    window.addEventListener('resize', this.onResize)
+  computed: {
+    ...mapState(['showGeogebra']),
   },
-  beforeUnmount() {
+  activated() {
+    console.log('activated')
+    window.addEventListener('resize', this.onResize)
+    this.onResize()
+    this.show = this.$store.state.showGeogebra
+  },
+  deactivated() {
+    console.log('deactivated')
     window.removeEventListener('resize', this.onResize)
   },
   methods: {
+    ...mapMutations(['setShowGeogebra']),
     onResize() {
-      this.w = window.innerWidth - 50
-      this.h = window.innerHeight - 200
-    }
+      this.w = window.innerWidth - 29
+      this.h = window.innerHeight -119
+      setTimeout(() => {
+        this.w = window.innerWidth - 30
+        this.h = window.innerHeight -120
+      }, 50)
+    },
+    scheduleShowHide(v) {
+      setTimeout(() => {
+        this.setShowGeogebra(v)
+        if (this.$route.path === '/') {
+          this.onResize()
+          this.show = v
+        }
+      }, 1000)
+    },
   }
 }
 </script>
---
+++ store/index.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+  showGeogebra: false,
+})
+
+export const mutations = {
+  setShowGeogebra: (state, value) => { state.showGeogebra = value },
+}
